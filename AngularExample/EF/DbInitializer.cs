dbb1b65ffae452f8962fc918acb1c452a6185eaa
Paweł Iżycki <pawelizycki@gmail.com>
2017-11-18 18:33:09 +0100

    Add EF Core

---
+++ AngularExample/EF/DbInitializer.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+
+namespace AngularExample.EF
+{
+    public static class DbInitializer
+    {
+        public static void Initialize(Context context)
+        {
+            context.Database.EnsureCreated();
+
+            if (context.Customers.Any())
+            {
+                return;
+            }
+
+            var companies = new[]
+            {
+                 
+            };
+
+            context.AddRange(companies);
+            context.SaveChanges();
+
+
+        }
+    }
+}
--- AngularExample/Program.cs
+++ AngularExample/Program.cs
@@ -1,11 +1,8 @@
 using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
+using AngularExample.EF;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
 namespace AngularExample
@@ -14,6 +11,23 @@ namespace AngularExample
     {
         public static void Main(string[] args)
         {
+            var host = BuildWebHost(args);
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var context = services.GetRequiredService<Context>();
+                    DbInitializer.Initialize(context);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred while seeding the database.");
+                }
+            }
+
             BuildWebHost(args).Run();
         }
 
