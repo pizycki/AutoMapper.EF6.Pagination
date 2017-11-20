37b6343c5d558246384ac94bf1c94c357cbc30c0
Paweł Iżycki <pawelizycki@gmail.com>
2017-11-20 22:21:25 +0100

    Add seed with customers

--- AngularExample/Controllers/SampleDataController.cs
+++ AngularExample/Controllers/SampleDataController.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using AngularExample.EF;
@@ -8,7 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace AngularExample.Controllers
 {
-    public class AllCompaniesQuery : IQueryWithPagination
+    public class AllCustomersQuery : IQueryWithPagination
     {
         public string OrderBy { get; set; }
         public bool Descending { get; set; }
@@ -26,45 +25,9 @@ namespace AngularExample.Controllers
         }
 
         [HttpGet, Route("api/companies")]
-        public IEnumerable<Customer> GetAllCompanies(AllCompaniesQuery query) =>
+        public IEnumerable<Customer> GetAllCompanies(AllCustomersQuery query) =>
             _context.Customers
                     .SortAndPaginate(query)
                     .ToList();
-    }
-
-    [Route("api/[controller]")]
-    public class SampleDataController : Controller
-    {
-        private static string[] Summaries = new[]
-        {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
-
-        [HttpGet("[action]")]
-        public IEnumerable<WeatherForecast> WeatherForecasts()
-        {
-            var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                DateFormatted = DateTime.Now.AddDays(index).ToString("d"),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
-            });
-        }
-
-        public class WeatherForecast
-        {
-            public string DateFormatted { get; set; }
-            public int TemperatureC { get; set; }
-            public string Summary { get; set; }
-
-            public int TemperatureF
-            {
-                get
-                {
-                    return 32 + (int)(TemperatureC / 0.5556);
-                }
-            }
-        }
     }
 }
