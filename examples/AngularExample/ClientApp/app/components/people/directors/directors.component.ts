6385e2b286510b92e573d68235b42af5e2d8f36e
Paweł Iżycki <pawelizycki@gmail.com>
2018-03-11 01:07:57 +0100

    Employers are not loading

--- examples/AngularExample/ClientApp/app/components/people/directors/directors.component.ts
+++ examples/AngularExample/ClientApp/app/components/people/directors/directors.component.ts
@@ -1,75 +1,56 @@
-import { Component, Inject } from "@angular/core";
+import { Component, Inject, HostListener } from "@angular/core";
 import { Http } from "@angular/http";
+import { Observable } from "rxjs/Observable";
 import { OnInit } from "@angular/core/src/metadata/lifecycle_hooks";
-import { PeopleProvider } from "providers/people.provider";
 import { IPage } from "shared/pagination";
+import { PeopleProvider } from "providers/people.provider";
 import { IPerson } from "components/people/models/person";
-
-class PageHelper {
-
-    static ReplaceItems<T>(ls1: T[], ls2: T[]): any {
-        return !!ls2 ? ls2 : ls1;
-    }
-
-    static AppendItems<T>(ls1: T[], ls2: T[]): any {
-        return ls1.concat(ls2);
-    }
-
-    /**
-     * Merges two objects of the same type.
-     * If property value is not present on second object, it takes value from first type. 
-     * @param p1 First object
-     * @param p2 Second object
-     */
-    static merge<T>(p1: IPage<T>, p2: IPage<T>): IPage<T> {
-        p2.number = !!p2.number ? p2.number : p1.number;
-        p2.size = !!p2.size ? p2.size : p1.size;
-        return p2;
-    }
-}
 
 @Component({
     selector: "directors",
     templateUrl: "./directors.component.html",
     providers: [PeopleProvider]
 })
-export class DirectorsComponent implements OnInit {
-
-    private pageSize: number = 10;
+export class DirectorsComponent {
 
     page: IPage<IPerson>;
-
-    get pageNumber(): number {
-        return !!this.page ? this.page.number : 1;
-    }
+    private orderBy = "Id";
 
     get items(): IPerson[] {
         return !!this.page ? this.page.items : [];
     }
 
-    get pagesTotal(): number {
-        return !!this.page ? this.page.pagesTotal : 0;
+    ngOnInit(): void {
+        this.page = {
+            number: 1,
+            size: 20,
+            items: [],
+            pagesTotal: 0
+        }
+
+        this.loadPage(1, true);
     }
 
-    public onPageSelected(page: number): void {
-        this.loadPage(page);
+    @HostListener("window:scroll", ["$event"])
+    onScroll(): void {
+        this.loadPage(this.page.number + 1);
     }
 
     private loadPage(page: number = 1, totalPages: boolean = false): void {
-        this.PeopleProvider
-            .customersPaginated(page, this.pageSize, totalPages)
-            .subscribe(result => {
-                let page = result.json();
-                if (!page.pagesTotal) {
-                    page.pagesTotal = this.pagesTotal;
-                }
-                this.page = page;
-            }, error => console.error(error));
+        if (this.canLoadData()) {
+            this.PeopleProvider
+                .getDirectors(page, this.page.size)
+                .subscribe(result => {
+                    let page = result.json();
+                    page.items = this.page.items.concat(page.items);
+                    this.page = page;
+                }, error => console.error(error));
+        }
     }
 
-    ngOnInit(): void {
-        this.loadPage(1, true);
-    }
+    private canLoadData = (): boolean =>
+        this.page.number !== this.page.pagesTotal
+        && (window.innerHeight + window.scrollY) >= document.body.offsetHeight;
 
     constructor(private PeopleProvider: PeopleProvider) {
     }
--- examples/AngularExample/ClientApp/app/providers/people.provider.ts
+++ examples/AngularExample/ClientApp/app/providers/people.provider.ts
@@ -10,10 +10,24 @@ export class PeopleProvider {
         @Inject("BASE_URL") private baseUrl: string) {
     }
 
-    customersPaginated(page: number, size: number, totalPages: boolean = false, orderBy: string = ""): Observable<any> {
+    getCustomers(page: number, size: number, totalPages: boolean = false): Observable<any> {
         let query = `api/customers?size=${size}&number=${page}`;
-        if (!!orderBy && orderBy !== "") query + `&orderby=${orderBy}`;
+        if (totalPages)
+            query + "&includeTotalPages";
+        return this.http.get(this.baseUrl + query);
+    }
+
+    /**
+     * @desc Gets next part of infinite page.
+     */
+    getDirectors = (part: number, size: number): Observable<any> =>
+        this.http.get(this.baseUrl + `api/directors?size=${size}&number=${part}`);
+
+    getEmployers(page: number, size: number, totalPages: boolean = false, orderBy: string, desc: boolean = false): Observable<any> {
+        let query = `api/employers?size=${size}&number=${page}`;
+        if (!!orderBy && orderBy !== "") query + `&orderBy=${orderBy}`;
         if (totalPages) query + "&includeTotalPages";
+        console.log("query", query);
         return this.http.get(this.baseUrl + query);
     }
 }
